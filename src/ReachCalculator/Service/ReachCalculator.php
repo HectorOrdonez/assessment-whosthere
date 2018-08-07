a1fcbacdd03209eaf9eb7e554b2a657100ad8434
Hector Ordonez <h.ordonez.garcia@gaspedaal.nl>
2018-08-07 12:29:33 +0200

    ReachCalculator reaching out for retweeters

--- src/ReachCalculator/Service/ReachCalculator.php
+++ src/ReachCalculator/Service/ReachCalculator.php
@@ -3,15 +3,47 @@
 namespace WhosThere\ReachCalculator\Service;
 
 use WhosThere\ReachCalculator\ReachCalculatorServiceInterface;
+use WhosThere\TwitterClient\TwitterClientInterface;
 
 class ReachCalculator implements ReachCalculatorServiceInterface
 {
+    /**
+     * @var TwitterClientInterface
+     */
+    private $client;
+
+    /**
+     * ReachCalculator constructor.
+     * @param TwitterClientInterface $client
+     */
+    public function __construct(TwitterClientInterface $client)
+    {
+        $this->client = $client;
+    }
+
     /**
      * @inheritdoc
      */
     public function calculate($url)
     {
-        return 0;
+        $retweeters = $this->getRetweeters($url);
+
+        $followers = $this->getFollowers($retweeters);
+
+        return count($retweeters) + count($followers);
     }
 
+    /**
+     * @param $url
+     * @return array
+     */
+    private function getRetweeters($url)
+    {
+        return $this->client->placeholder($url);
+    }
+
+    private function getFollowers(array $retweeters)
+    {
+        return [];
+    }
 }
--- tests/Feature/ReachCalculator/ReachCalculatorServiceTest.php
+++ tests/Feature/ReachCalculator/ReachCalculatorServiceTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use WhosThere\ReachCalculator\Service\ReachCalculator;
 use WhosThere\ReachCalculator\ReachCalculatorServiceInterface;
+use WhosThere\TwitterClient\TwitterClientInterface;
 
 class ReachCalculatorServiceTest extends FeatureTestCase
 {
@@ -12,7 +13,7 @@ class ReachCalculatorServiceTest extends FeatureTestCase
      */
     public function it_instantiates()
     {
-        $service = new ReachCalculator();
+        $service = new ReachCalculator($this->getMockedClient());
 
         $this->assertInstanceOf(ReachCalculatorServiceInterface::class, $service);
     }
@@ -23,13 +24,65 @@ class ReachCalculatorServiceTest extends FeatureTestCase
     public function it_returns_0_when_no_retweets()
     {
         // Arrange
+        $client = $this->getMockedClient();
+        $client->shouldReceive('placeholder')->andReturn([]);
         $url = $this->faker()->url;
-        $service = new ReachCalculator();
+        $service = new ReachCalculator($client);
 
         // Act
         $result = $service->calculate($url);
 
         // Assert
+        $client->shouldHaveReceived('placeholder')->with($url);
         $this->assertEquals(0, $result);
+    }
+
+    /**
+     * @test
+     */
+    public function it_returns_1_when_1_retweeter_without_followers()
+    {
+        // Arrange
+        $client = $this->getMockedClient();
+        $client->shouldReceive('placeholder')->andReturn(['something']);
+
+        $url = $this->faker()->url;
+        $service = new ReachCalculator($client);
+
+        // Act
+        $result = $service->calculate($url);
+
+        // Assert
+        $client->shouldHaveReceived('placeholder')->with($url);
+        $this->assertEquals(1, $result);
+    }
+
+
+    /**
+     * @test
+     */
+    public function it_returns_3_when_1_retweeter_with_2_followers()
+    {
+        // Arrange
+        $client = $this->getMockedClient();
+        $client->shouldReceive('placeholder')->andReturn(['something']);
+
+        $url = $this->faker()->url;
+        $service = new ReachCalculator($client);
+
+        // Act
+        $result = $service->calculate($url);
+
+        // Assert
+        $client->shouldHaveReceived('placeholder')->with($url);
+        $this->assertEquals(3, $result);
+    }
+
+    /**
+     * @return \Mockery\MockInterface|TwitterClientInterface
+     */
+    private function getMockedClient()
+    {
+        return \Mockery::mock(TwitterClientInterface::class);
     }
 }
